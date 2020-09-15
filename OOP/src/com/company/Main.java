1c340aa671e131169b0e1f107fac451f069bdc30
Duy Anh <duy.nguyen@sarake.fi>
2020-09-15 12:40:40 +0300

    OOP Challenge

--- OOP/src/com/company/Main.java
+++ OOP/src/com/company/Main.java
@@ -3,6 +3,22 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	// write your code her
+        Hamburger hamburger = new Hamburger("Basic", "Sausage", 3.56, "White");
+        double price = hamburger.itemizeHamburger();
+        hamburger.addHamburgerAddition1("Tomato", 0.27);
+        hamburger.addHamburgerAddition2("Lettuce", 0.75);
+        hamburger.addHamburgerAddition3("Cheese", 1.13);
+        System.out.println("Total burger price is " + hamburger.itemizeHamburger());
+
+        HealthyBurger healthyBurger = new HealthyBurger("Bacon", 5.67);
+        healthyBurger.itemizeHamburger();
+        healthyBurger.addHamburgerAddition1("Egg", 5.43);
+        healthyBurger.itemizeHamburger();
+        healthyBurger.addHealthAddition1("Lentils", 3.41);
+        System.out.println("Total burger price is " + healthyBurger.itemizeHamburger());
+
+        DeluxeBurger deluxeBurger = new DeluxeBurger();
+        deluxeBurger.itemizeHamburger();
     }
 }
